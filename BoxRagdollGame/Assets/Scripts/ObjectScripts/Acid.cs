1d068edfcfe869ac7e1e5a18e6dd39d2daaceeca
Barrel <visvam64@gmail.com>
2021-08-16 21:35:05 -0400

    Worked On Acid, Platform, And LevelManager
    - Acid Script now disables Balence and PlayerController Script on Impact
    - Platform has been added, Not used yet
    - Added Empty LevelManager Script

--- BoxRagdollGame/Assets/Scripts/ObjectScripts/Acid.cs
+++ BoxRagdollGame/Assets/Scripts/ObjectScripts/Acid.cs
@@ -1,18 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class Acid : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void OnTriggerEnter2D(Collider2D col)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (col.gameObject.name == "Hips")
+        {
+            col.gameObject.GetComponent<Balence>().enabled = false;
+            col.gameObject.GetComponentInParent<PlayerController>().enabled = false;
+        }
     }
 }
