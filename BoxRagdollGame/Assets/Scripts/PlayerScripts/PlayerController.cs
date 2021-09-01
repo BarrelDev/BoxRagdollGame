2196091d7a8d351b81de9791722649879f99ab05
Barrel <visvam64@gmail.com>
2021-09-01 10:56:15 -0400

    Various Changes
    - Acid is now a prefab
    - Increased Height of Acid
    - Moved SeeSaw in Level0 for easier level completion
    - ExitDoor now has a new BoxCollider2D
    - Added the Ability to Exit the game on clicking "Escape"
    - Created the first development build of the game.

--- BoxRagdollGame/Assets/Scripts/PlayerScripts/PlayerController.cs
+++ BoxRagdollGame/Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -30,6 +30,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKey(KeyCode.Escape)) 
+        {
+            Application.Quit();
+        }
+
         //Debug.Log(Input.GetAxis("Horizontal").ToString());
         if (Input.GetAxisRaw("Horizontal") != 0)
         {
